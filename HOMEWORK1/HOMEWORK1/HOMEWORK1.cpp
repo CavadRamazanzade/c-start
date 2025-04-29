7bdfb5fc10302ff9e7fb484257f672841f869795
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-04-29 18:38:12 +0400

    homework1

---
+++ HOMEWORK1/HOMEWORK1/HOMEWORK1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	/*int num1 = 0;
+	int num2 = 0;
+	int num3 = 0;
+	 cout << "Enter number1 ", cin >> num1;
+	 cout << "Enter number2 ", cin >> num2;
+	 cout << "Enter number3 ", cin >> num3;
+	 if (num1<num2 || num1<num3)
+	 {
+		 cout << num1 << endl;
+	 }
+	 else if (num2<num1 || num2<num3)
+	 {
+		 cout << num2 << endl;
+	 }
+	 else
+	 {
+		 cout << num3 << endl;
+	 }*/
+
+}
+
+
+
+
+
+
+
+
+
+   /* int num1 = 0;
+	cout << "Enter your age: ", cin >> num1;
+	if (num1<18)
+	{
+		cout << "You are under 18 years-exit" << endl;
+	}
+	else if (num1>=18)
+	{
+		cout << "You are 18 or older" << endl;*/
+	
+
+
+
+
+
+
+
