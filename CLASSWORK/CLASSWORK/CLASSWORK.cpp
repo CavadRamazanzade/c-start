bc1ee7e099ed3e47bbbae853868c31125031884c
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-05-10 01:44:43 +0400

    HomeWork

---
+++ CLASSWORK/CLASSWORK/CLASSWORK.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//int num1 = 0;
+	//
+	//
+	//cout << "num1:", cin >> num1;
+	//
+	//if (num1> 90)
+	//{
+	//	cout << "A" << endl;
+	//}
+	// else if (num1> 80 and num1<90)
+	//{
+	//	cout << "B" << endl;
+	//}
+	//else if (num1> 70 and num1< 80)
+	//{
+	//	cout << "C" << endl;
+	//}
+	//else if (num1>60 and num1 < 70)
+	//{
+	//	cout << "D" << endl;
+	//}
+	//else
+	//{
+	//	cout << "F" << endl;
+	//}
+
+
+
+
+	/*string letter = "";
+	cout << "letter:", cin >> letter;
+	if (letter == "a" or letter == "e" || letter == "i" or letter == "u" or letter == "o")
+	{
+		cout << "glasnaya" << endl;
+	}
+	else
+	{
+		cout << "soglasnaya" << endl;
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	/*
+	int num1 = 0;
+	cout << "Entrer num :", cin >> num1;
+	if (num1 % 5==0 and num1 % 11 == 0)
+	{
+		cout << "Delitsa i na 5 i na 11" << endl;
+	}
+	else
+	{
+		cout << "Error" << endl;
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	/*int num1 = 0;
+	int num2 = 0;
+	cout << "Enter num1 :", cin >> num1;
+	cout << "Enter num2 :", cin >> num2;
+	if (num1>num2)
+	{
+		cout << num1 << endl;
+	}
+	else
+	{
+		cout << num2 << endl;
+	}*/
+	
+	
+	
+	
+	
+	
+	
+	
+	/*int num1 = 0;
+	cout << "Enter num:", cin >> num1;
+		if (num1 == 0)
+		{
+			cout << 0 << endl;
+		}
+		else if (num1 > 0)
+		{
+			cout << "polojitelnoe" << endl;
+		}
+		else
+		{
+			cout << "otricatelnoe" << endl;
+		}*/
+
+
+
+
+
+
+
+
+}
+
+		
---
+++ ForHomework/ForHomework/ForHomework.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	// ya smog tolko eti zadaniya sdelat 
+
+
+
+	
+	/*int result = 0;
+
+		for (int i = 2; i <= 100; i += 2) {
+			result += i;
+		}
+		cout <<result<<endl;*/
+
+
+
+	/*for (int i = 0; i <= 5; i++)
+	{
+		cout << "Enter number :", cin >> i;
+		for (int j = 0; j <= 5; j++)
+		{
+	     cout << "Enter number :" <<" * "<< j;
+		}
+		cout << endl;
+	}*/
+
+
+
+
+
+	/*int num = 0;
+	cout << "Enter number :", cin >> num;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << num << " x " << i << " == " << num * i << endl;
+	}*/
+
+
+	/*int i = 0;
+	for (int i = 1; i < 50; i++)
+	{
+		if (i % 2 == 0)
+		{
+			cout << i << endl;
+		}
+
+	}*/
+
+
+
+
+
+
+    /*int i = 0;
+	for (int i = 1; i < 50; i++)
+	{
+		cout << i << endl;
+		
+	}*/
+	
+
+
+
+		
+}
+
