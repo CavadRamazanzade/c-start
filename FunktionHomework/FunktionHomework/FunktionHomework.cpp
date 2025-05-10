efc6e36319cb61cc7e40d713f9058e76ebd743f7
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-05-10 17:58:43 +0400

    FunktionHomework

---
+++ FunktionHomework/FunktionHomework/FunktionHomework.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Ya ne mogu sdelat dz 
+
+
+
+	int num = 0;
+	int i = 0;
+	cout << "Enter number: ", cin >> num;
+	for (int i = 0; i + num; i++)
+	{
+		while (num < 0)
+		{
+			int test = num % 10;
+			cout << test << endl;
+			num = num / 10;
+
+		}
+		cout << num << endl;
+	}
+
+
+
+
+
+
+	//Dz N 1 
+	/*int num1 = 0;
+	int num2 = 0;
+	cout << "Enter number1 : ", cin >> num1;
+	cout << "Enter number2 : ", cin >> num2;
+	int max = (num1 > num2) ? num1 : num2;
+	cout << max << endl;
+
+	if (num1 > num2)
+	{
+		cout << num1 << endl;
+	}
+	else {
+		cout << num2 << endl;
+
+	}*/
+
+}
+
+
