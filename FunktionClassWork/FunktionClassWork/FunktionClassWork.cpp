f9088a7b2b98e6054e5f9ebf5ea0b26fc88271a1
CavadRamazanzade <ramaz.vb00@gmail.com>
2025-05-10 17:28:38 +0400

    ClassWork

---
+++ FunktionClassWork/FunktionClassWork/FunktionClassWork.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+//bool Condition(int num) {
+//	return false;
+//}
+//
+//
+//
+//
+//int Plus(int num1, int num2)
+//{
+//	return num1 + num2;
+//}
+//
+//
+//int Minus(int num1, int num2)
+//{
+//	if (num1>num2)
+//	{
+//	
+//		return num1 - num2;
+//	}
+//	
+//	return num2 - num1;
+//	
+//}
+//
+//
+//int Multiply(int num1,int num2)
+//{
+//	return num1 * num2;
+//}
+//
+//float Divide(int num1,int num2)
+//{
+//	float res = 0;
+//	if (num1 > num2)
+//	{
+//		res = num1 / num2;
+//		return res;
+//	}
+//	res = num2 / num1;
+//	return res;
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	cout << "Enter number1 : ", cin >> num1;
+//	cout << "Enter number2 : ", cin >> num2;
+//	int resPlus = Plus(num1, num2);
+//	int resMinus = Minus(num1, num2);
+//	float resDivide = Divide(num1, num2);
+//	int resMultiply = Multiply(num1, num2);
+//	cout << resPlus << endl; 
+//	cout << resMinus << endl; 
+//	cout << resDivide << endl; 
+//	cout << resMultiply << endl; 
+//	cout << Condition(num2) << endl; 
+//
+//}
